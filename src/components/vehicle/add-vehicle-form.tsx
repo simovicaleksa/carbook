33cb84d87b4f1136abffabb902ef8631111327c9
aleksa_simovic <102186502+simovicaleksa@users.noreply.github.com>
2025-01-19 22:50:07 +0100

    feat: add form structure to AddVehicleForm

--- src/components/vehicle/add-vehicle-form.tsx
+++ src/components/vehicle/add-vehicle-form.tsx
@@ -5,8 +5,12 @@ import { type z } from "zod";
 
 import { addVehicleSchema } from "~/app/actions/vehicle-validators";
 
-import { useLoading } from "~/hooks/use-loading";
+import { cn } from "~/lib/utils";
 
+import { useLoading } from "~/hooks/use-loading";
+import { useIsMobile } from "~/hooks/use-mobile";
+
+import MakeSelect from "../input/make-select";
 import VehicleTypeRadio from "../input/vehicle-type-radio";
 import { AlertDialogCancel, AlertDialogFooter } from "../ui/alert-dialog";
 import { Button } from "../ui/button";
@@ -18,13 +22,17 @@ import {
   FormLabel,
   FormMessage,
 } from "../ui/form";
+import { Input } from "../ui/input";
 import LoadingButton from "../ui/loading-button";
 
 export default function AddVehicleForm() {
   const loading = useLoading();
+  const isMobile = useIsMobile();
 
   async function onSubmit(values: z.infer<typeof addVehicleSchema>) {
     loading.start();
+
+    console.log(values);
 
     loading.end();
   }
@@ -33,22 +41,27 @@ export default function AddVehicleForm() {
     resolver: zodResolver(addVehicleSchema),
     defaultValues: {
       type: "car",
-      make: "hello",
-      model: "hello",
-      year: 2022,
-      distanceTraveled: 123,
+      make: "",
+      model: "",
+      year: 0,
+      distanceTraveled: 0,
     },
   });
 
   return (
     <div>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className={cn("grid grid-cols-2 gap-2 space-y-5", {
+            "grid-cols-1": isMobile,
+          })}
+        >
           <FormField
             control={form.control}
             name="type"
             render={({ field }) => (
-              <FormItem>
+              <FormItem className="col-span-full">
                 <FormLabel>Type</FormLabel>
                 <FormControl>
                   <VehicleTypeRadio
@@ -60,7 +73,70 @@ export default function AddVehicleForm() {
               </FormItem>
             )}
           />
-          <AlertDialogFooter>
+          <FormField
+            control={form.control}
+            name="make"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Make</FormLabel>
+                <FormControl>
+                  <MakeSelect
+                    onValueChange={field.onChange}
+                    value={field.value}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="model"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Model</FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder="Vehicle model..." />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="year"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Year</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    placeholder="Vehicle year..."
+                    type="number"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="distanceTraveled"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Distance traveled</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    placeholder="Distance traveled..."
+                    type="number"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <AlertDialogFooter className="col-span-full">
             <AlertDialogCancel asChild>
               <Button variant={"outline"} type="button">
                 Cancel
