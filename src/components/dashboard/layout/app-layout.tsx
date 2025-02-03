bf3b31a4850d7c72be780b5ed774d350fbe05906
aleksa_simovic <102186502+simovicaleksa@users.noreply.github.com>
2025-02-03 15:33:05 +0100

    style: make layout header sticky

--- src/components/dashboard/layout/app-layout.tsx
+++ src/components/dashboard/layout/app-layout.tsx
@@ -10,7 +10,7 @@ export function AppLayoutHeader({
   action?: React.ReactNode;
 }) {
   return (
-    <div className="flex w-full flex-row items-center justify-between border-b bg-sidebar px-5 py-5">
+    <div className="sticky top-0 z-50 flex w-full flex-row items-center justify-between border-b bg-sidebar px-5 py-5 shadow-sm">
       <h1 className="font-mono text-xl font-medium">{title}</h1>
       {action}
     </div>
