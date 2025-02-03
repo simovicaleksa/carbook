2296d93f19881d5d4e6ef2a1766b16a52e5ed5f5
aleksa_simovic <102186502+simovicaleksa@users.noreply.github.com>
2025-02-03 13:16:08 +0100

    refactor: "options" to "eventTypes" and own folder

--- src/components/input/event-type-radio.tsx
+++ src/components/input/event-type-radio.tsx
@@ -1,90 +1,10 @@
 import React from "react";
 
 import * as RadioGroup from "@radix-ui/react-radio-group";
-import {
-  CircleCheck,
-  CircleEllipsis,
-  Coins,
-  Drill,
-  Droplets,
-  Fuel,
-  Hammer,
-  Milestone,
-  RefreshCw,
-  Search,
-  TriangleAlert,
-  Wrench,
-  Zap,
-} from "lucide-react";
+import { CircleCheck } from "lucide-react";
 
+import { eventTypes } from "~/lib/data/event-types";
 import { cn } from "~/lib/utils";
-
-const options = [
-  {
-    value: "refuel",
-    label: "Refuel",
-    icon: <Fuel className="mb-2 size-7 text-muted-foreground sm:size-5" />,
-  },
-  {
-    value: "service",
-    label: "Service",
-    icon: <Wrench className="mb-2 size-7 text-muted-foreground sm:size-5" />,
-  },
-  {
-    value: "repair",
-    label: "Repair",
-    icon: <Hammer className="mb-2 size-7 text-muted-foreground sm:size-5" />,
-  },
-  {
-    value: "replacement",
-    label: "Replacement",
-    icon: <RefreshCw className="mb-2 size-7 text-muted-foreground sm:size-5" />,
-  },
-  {
-    value: "purchase",
-    label: "Purchase",
-    icon: <Coins className="mb-2 size-7 text-muted-foreground sm:size-5" />,
-  },
-  {
-    value: "inspection",
-    label: "Inspection",
-    icon: <Search className="mb-2 size-7 text-muted-foreground sm:size-5" />,
-  },
-  {
-    value: "tune-up",
-    label: "Tune-up",
-    icon: <Zap className="mb-2 size-7 text-muted-foreground sm:size-5" />,
-  },
-  {
-    value: "wash",
-    label: "Wash",
-    icon: <Droplets className="mb-2 size-7 text-muted-foreground sm:size-5" />,
-  },
-  {
-    value: "milestone",
-    label: "Milestone",
-    icon: <Milestone className="mb-2 size-7 text-muted-foreground sm:size-5" />,
-  },
-  {
-    value: "upgrade",
-    label: "Upgrade",
-    icon: <Drill className="mb-2 size-7 text-muted-foreground sm:size-5" />,
-  },
-  {
-    value: "accident",
-    label: "Accident",
-    icon: (
-      <TriangleAlert className="mb-2 size-7 text-muted-foreground sm:size-5" />
-    ),
-  },
-  {
-    value: "other",
-    label: "Other",
-    icon: (
-      <CircleEllipsis className="mb-2 size-7 text-muted-foreground sm:size-5" />
-    ),
-  },
-];
 
 export default function EventTypeRadio({
   className,
@@ -95,7 +15,7 @@ export default function EventTypeRadio({
       className={cn("grid w-full grid-cols-2 gap-4 sm:grid-cols-3", className)}
       {...rest}
     >
-      {options.map((option) => (
+      {eventTypes.map((option) => (
         <RadioGroup.Item
           key={option.value}
           value={option.value}
