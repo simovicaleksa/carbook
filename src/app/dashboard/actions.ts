a47dc2e07c4cfb8213a9592106dea953c68836ad
aleksa_simovic <102186502+simovicaleksa@users.noreply.github.com>
2025-02-03 12:58:30 +0100

    refactor: move history actions to own folder

--- src/app/dashboard/actions.ts
+++ src/app/dashboard/actions.ts
@@ -1,13 +1,12 @@
 "use server";
 
-import { and, desc, eq, lt, gt } from "drizzle-orm";
+import { eq } from "drizzle-orm";
 import { type z, ZodError } from "zod";
 
 import { db } from "~/db";
-import { historyTable, userProfileTable, vehicleTable } from "~/db/_schema";
+import { userProfileTable } from "~/db/_schema";
 
 import { AuthorizationError, authorize } from "~/lib/server/authorize";
-import { createHistoryEvent } from "~/lib/server/history";
 import {
   getUserProfileFromUserId,
   updateUserProfileSelectedVehicle,
@@ -19,7 +18,6 @@ import {
   getVehiclesFromUserId,
 } from "~/lib/server/vehicle";
 
-import { addHistoryEventSchema } from "../actions/history-validators";
 import { addVehicleSchema } from "../actions/vehicle-validators";
 
 export async function getUserVehicles() {
@@ -194,113 +192,6 @@ export async function updateUserSelectedVehicle(vehicleId: string) {
       return {
         ok: false,
         status: 401,
-        error: error,
-      };
-    return {
-      ok: false,
-      status: 500,
-      error: error as Error,
-    };
-  }
-}
-
-export async function createUserHistoryEvent(
-  vehicleId: string,
-  newEvent: z.infer<typeof addHistoryEventSchema>,
-) {
-  try {
-    await authorize(async (user) => {
-      const vehicle = await db.query.vehicleTable.findFirst({
-        where: and(
-          eq(vehicleTable.id, vehicleId),
-          eq(vehicleTable.ownerId, user.id),
-        ),
-      });
-
-      if (!vehicle) return false;
-
-      return true;
-    });
-
-    addHistoryEventSchema.parse(newEvent);
-
-    // Check for preceding events with higher distance traveled
-    const precedingConflict = await db
-      .select()
-      .from(historyTable)
-      .where(
-        and(
-          eq(historyTable.vehicleId, vehicleId),
-          lt(historyTable.date, newEvent.date),
-          gt(historyTable.atDistanceTraveled, newEvent.atDistanceTraveled),
-        ),
-      )
-      .limit(1);
-
-    if (precedingConflict.length > 0) {
-      return {
-        ok: false,
-        status: 400,
-        error: new Error("Older entry with higher distance traveled exists"),
-      };
-    }
-
-    // Check for upcoming events with lower distance traveled
-    const upcomingConflict = await db
-      .select()
-      .from(historyTable)
-      .where(
-        and(
-          eq(historyTable.vehicleId, vehicleId),
-          gt(historyTable.date, newEvent.date),
-          lt(historyTable.atDistanceTraveled, newEvent.atDistanceTraveled),
-        ),
-      )
-      .limit(1);
-
-    if (upcomingConflict.length > 0) {
-      return {
-        ok: false,
-        status: 400,
-        error: new Error("Newer entry with lower distance traveled exists"),
-      };
-    }
-
-    const latestEvent = await db.query.historyTable.findFirst({
-      orderBy: desc(historyTable.atDistanceTraveled),
-      where: and(eq(historyTable.vehicleId, vehicleId)),
-    });
-
-    // update current vehicle distanceTraveled
-    if (
-      !latestEvent ||
-      latestEvent.atDistanceTraveled < newEvent.atDistanceTraveled
-    ) {
-      await db
-        .update(vehicleTable)
-        .set({
-          distanceTraveled: newEvent.atDistanceTraveled,
-        })
-        .where(eq(vehicleTable.id, vehicleId));
-    }
-
-    await createHistoryEvent(vehicleId, newEvent);
-
-    return {
-      ok: true,
-      status: 200,
-    };
-  } catch (error) {
-    if (error instanceof AuthorizationError)
-      return {
-        ok: false,
-        status: 401,
-        error: error,
-      };
-    if (error instanceof ZodError)
-      return {
-        ok: false,
-        status: 400,
         error: error,
       };
     return {
