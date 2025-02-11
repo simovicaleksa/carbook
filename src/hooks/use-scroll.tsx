f471147eef03790753ef71e7b44434e1343b2e41
aleksa_simovic <102186502+simovicaleksa@users.noreply.github.com>
2025-02-11 13:40:54 +0100

    feat: add use scroll helper hook

---
+++ src/hooks/use-scroll.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export function useScroll() {
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return { scrollY };
+}
