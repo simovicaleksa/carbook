8316fb4440ea44dd0d976e25e6eb8bf05b9275b8
aleksa_simovic <102186502+simovicaleksa@users.noreply.github.com>
2025-02-11 14:00:26 +0100

    fix: hydration window issue

--- src/components/dashboard/layout/back-to-top.tsx
+++ src/components/dashboard/layout/back-to-top.tsx
@@ -11,17 +11,15 @@ import { Button } from "~/components/ui/button";
 export default function BackToTop() {
   const { scrollY } = useScroll();
 
-  if (typeof window === "undefined") return null;
-
   return (
     <Button
       size={"icon"}
       aria-label="Back to top"
       onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
       className={cn(
-        "pointer-events-none fixed bottom-5 right-5 aspect-square size-fit rounded-full p-3 opacity-0 shadow transition-opacity duration-200",
+        "pointer-events-none fixed bottom-0 right-0 z-50 aspect-square size-fit scale-125 rounded-full p-3 opacity-0 shadow transition-all duration-200",
         {
-          "pointer-events-auto opacity-100": scrollY > 200,
+          "pointer-events-auto bottom-7 right-7 opacity-100": scrollY > 200,
         },
       )}
     >
