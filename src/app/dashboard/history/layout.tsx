c21a4a47251f0290dbb1c85092d16bdaf7fba193
aleksa_simovic <102186502+simovicaleksa@users.noreply.github.com>
2025-02-12 14:13:07 +0100

    fix: move fetch logic from static layout to dynamic page

--- src/app/dashboard/history/layout.tsx
+++ src/app/dashboard/history/layout.tsx
@@ -1,8 +1,5 @@
-import { notFound } from "next/navigation";
-
 import { DeleteEventDialogProvider } from "~/context/delete-event-dialog-context";
 import { EditHistoryDialogProvider } from "~/context/edit-history-dialog-context";
-import { EventsProvider } from "~/context/events-context";
 import { InspectEventDialogProvider } from "~/context/inspect-event-dialog-context";
 import { SelectedEventProvider } from "~/context/selected-event-context";
 
@@ -10,35 +7,23 @@ import DeleteEventDialog from "~/components/history/delete-event-dialog";
 import EditHistoryDialog from "~/components/history/edit-event-dialog";
 import InspectEventDialog from "~/components/history/inspect-event-dialog";
 
-import { getCurrentSelectedVehicle } from "../actions";
-
-import { getVehicleHistoryEvents } from "./actions";
-
-export default async function HistoryLayout({
+export default function HistoryLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { data: selectedVehicle } = await getCurrentSelectedVehicle();
-
-  if (!selectedVehicle) return notFound();
-
-  const { data: events } = await getVehicleHistoryEvents(selectedVehicle.id);
-
   return (
-    <EventsProvider defaultValue={events ?? []}>
-      <SelectedEventProvider>
-        <EditHistoryDialogProvider>
-          <DeleteEventDialogProvider>
-            <InspectEventDialogProvider>
-              <InspectEventDialog />
-              <EditHistoryDialog />
-              <DeleteEventDialog />
-              {children}
-            </InspectEventDialogProvider>
-          </DeleteEventDialogProvider>
-        </EditHistoryDialogProvider>
-      </SelectedEventProvider>
-    </EventsProvider>
+    <SelectedEventProvider>
+      <EditHistoryDialogProvider>
+        <DeleteEventDialogProvider>
+          <InspectEventDialogProvider>
+            <InspectEventDialog />
+            <EditHistoryDialog />
+            <DeleteEventDialog />
+            {children}
+          </InspectEventDialogProvider>
+        </DeleteEventDialogProvider>
+      </EditHistoryDialogProvider>
+    </SelectedEventProvider>
   );
 }
--- src/app/dashboard/history/page.tsx
+++ src/app/dashboard/history/page.tsx
@@ -1,21 +1,57 @@
+import { notFound } from "next/navigation";
+
+import { EventsProvider } from "~/context/events-context";
+
 import {
   AppLayout,
   AppLayoutContent,
+  AppLayoutFooter,
   AppLayoutHeader,
 } from "~/components/dashboard/layout/app-layout";
+import AppPagination from "~/components/dashboard/layout/app-pagination";
 import AddHistoryEventHeaderButton from "~/components/history/add-history-event-header-button";
 import Timeline from "~/components/history/timeline";
 
-export default function History() {
+import { getCurrentSelectedVehicle } from "../actions";
+
+import { getVehicleHistoryEvents } from "./actions";
+
+const PER_PAGE = 20;
+
+export default async function History({
+  searchParams,
+}: {
+  searchParams: {
+    page?: number;
+  };
+}) {
+  const { data: selectedVehicle } = await getCurrentSelectedVehicle();
+
+  if (!selectedVehicle) return notFound();
+
+  const { data: historyEvents } = await getVehicleHistoryEvents(
+    selectedVehicle.id,
+    searchParams?.page ?? 1,
+    PER_PAGE,
+  );
+
   return (
-    <AppLayout>
-      <AppLayoutHeader
-        title="History"
-        action={<AddHistoryEventHeaderButton />}
-      />
-      <AppLayoutContent>
-        <Timeline />
-      </AppLayoutContent>
-    </AppLayout>
+    <EventsProvider
+      events={historyEvents?.events ?? []}
+      total={historyEvents?.total ?? 0}
+    >
+      <AppLayout>
+        <AppLayoutHeader
+          title="History"
+          action={<AddHistoryEventHeaderButton />}
+        />
+        <AppLayoutContent>
+          <Timeline />
+        </AppLayoutContent>
+        <AppLayoutFooter>
+          <AppPagination perPage={PER_PAGE} />
+        </AppLayoutFooter>
+      </AppLayout>
+    </EventsProvider>
   );
 }
