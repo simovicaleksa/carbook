e83853b38bceee63fa07306af2acf7656c9ae634
Aleksa Simovic <asesa1312@gmail.com>
2025-02-19 18:17:46 +0100

    fix: vehicle switcher dropdown menu logo

--- src/components/dashboard/sidebar/vehicle-switcher.tsx
+++ src/components/dashboard/sidebar/vehicle-switcher.tsx
@@ -32,12 +32,8 @@ export default function VehicleSwitcher() {
   const isMobile = useIsMobile();
   const { toggleOpen } = useAddVehicleDialog();
   const { vehicles } = useUserVehicles();
-  const { selectedVehicle, changeSelectedVehicle } = useSelectedVehicle();
+  const { selectedVehicle } = useSelectedVehicle();
   const Icon = getBrandIcon(selectedVehicle);
-
-  const handleVehicleChange = async (vehicle: VehicleType) => {
-    await changeSelectedVehicle(vehicle);
-  };
 
   return (
     <SidebarMenu>
@@ -72,35 +68,7 @@ export default function VehicleSwitcher() {
               Vehicles
             </DropdownMenuLabel>
             {vehicles.map((vehicle) => (
-              <DropdownMenuItem
-                key={vehicle.id}
-                className="cursor-pointer gap-2 p-2 font-medium"
-                onClick={() => handleVehicleChange(vehicle)}
-                disabled={selectedVehicle?.id === vehicle.id}
-              >
-                <div
-                  className={cn(
-                    "flex size-7 items-center justify-center rounded-[var(--radius)] p-4 *:!size-6",
-                    {
-                      "bg-primary text-primary-foreground":
-                        selectedVehicle?.id === vehicle.id,
-                    },
-                  )}
-                >
-                  <Icon />
-                </div>
-
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-normal">
-                    <span className="font-semibold">{vehicle.make}</span>
-                    {" - "}
-                    {vehicle.model}
-                  </span>
-                  <span className="truncate text-xs font-light italic">
-                    {vehicle.year}
-                  </span>
-                </div>
-              </DropdownMenuItem>
+              <VehicleSwitcherItem vehicle={vehicle} key={vehicle.id} />
             ))}
             <DropdownMenuSeparator />
             <DropdownMenuItem className="gap-2 p-2" onClick={toggleOpen}>
@@ -115,5 +83,47 @@ export default function VehicleSwitcher() {
         </DropdownMenu>
       </SidebarMenuItem>
     </SidebarMenu>
+  );
+}
+
+function VehicleSwitcherItem(props: { vehicle: VehicleType }) {
+  const { selectedVehicle, changeSelectedVehicle } = useSelectedVehicle();
+  const vehicle = props.vehicle;
+  const Icon = getBrandIcon(vehicle);
+
+  const handleVehicleChange = async (vehicle: VehicleType) => {
+    await changeSelectedVehicle(vehicle);
+  };
+
+  return (
+    <DropdownMenuItem
+      key={vehicle.id}
+      className="cursor-pointer gap-2 p-2 font-medium"
+      onClick={() => handleVehicleChange(vehicle)}
+      disabled={selectedVehicle?.id === vehicle.id}
+    >
+      <div
+        className={cn(
+          "flex size-7 items-center justify-center rounded-[var(--radius)] p-4 *:!size-6",
+          {
+            "bg-primary text-primary-foreground":
+              selectedVehicle?.id === vehicle.id,
+          },
+        )}
+      >
+        <Icon />
+      </div>
+
+      <div className="grid flex-1 text-left text-sm leading-tight">
+        <span className="truncate font-normal">
+          <span className="font-semibold">{vehicle.make}</span>
+          {" - "}
+          {vehicle.model}
+        </span>
+        <span className="truncate text-xs font-light italic">
+          {vehicle.year}
+        </span>
+      </div>
+    </DropdownMenuItem>
   );
 }
