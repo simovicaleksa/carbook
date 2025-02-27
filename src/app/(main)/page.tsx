2ef6dd8134f3c9b34e97c065707274a2cc078217
Aleksa Simovic <asesa1312@gmail.com>
2025-02-27 22:16:02 +0100

    feat: enable caching in HomePage component for improved performance

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,9 @@
+"use cache";
+
 import Features from "~/components/www/features/features";
 import Hero from "~/components/www/hero/hero";
 
-export default function HomePage() {
+export default async function HomePage() {
   return (
     <main>
       <Hero />
