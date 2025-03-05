b290b0220517d8b0849fa93879df7a72166715c6
Aleksa Simovic <asesa1312@gmail.com>
2025-03-05 16:56:28 +0100

    feat: include history filters skeleton in loading

--- src/app/dashboard/history/loading.tsx
+++ src/app/dashboard/history/loading.tsx
@@ -4,6 +4,7 @@ import {
   AppLayoutHeader,
 } from "~/components/dashboard/layout/app-layout";
 import AddHistoryEventHeaderButton from "~/components/history/add-history-event-header-button";
+import HistoryFiltersSkeleton from "~/components/skeleton/history-filters-skeleton";
 import TimelineSkeleton from "~/components/skeleton/timeline-skeleton";
 
 export default async function History() {
@@ -14,6 +15,7 @@ export default async function History() {
         action={<AddHistoryEventHeaderButton disabled />}
       />
       <AppLayoutContent>
+        <HistoryFiltersSkeleton />
         <TimelineSkeleton />
       </AppLayoutContent>
     </AppLayout>
