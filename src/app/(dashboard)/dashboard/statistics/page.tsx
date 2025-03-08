6264a443c7c8c5eadafc8a1e69c54be5c639be85
Aleksa Simovic <asesa1312@gmail.com>
2025-03-08 10:02:49 +0100

    feat: create statistics page route

---
+++ src/app/(dashboard)/dashboard/statistics/page.tsx
@@ -0,0 +1,14 @@
+import {
+  AppLayout,
+  AppLayoutContent,
+  AppLayoutHeader,
+} from "~/components/dashboard/layout/app-layout";
+
+export default function StatisticsPage() {
+  return (
+    <AppLayout>
+      <AppLayoutHeader title="Statistics" />
+      <AppLayoutContent></AppLayoutContent>
+    </AppLayout>
+  );
+}
